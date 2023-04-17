0698f767ae810edbeaac25b7079c453f24cf3f97
Achala Athukorala <chhathuranga@gmail.com>
2023-04-17 17:32:36 +0800

    Change shared pose from corrected to odometric

--- src/slam_toolbox_multirobot.cpp
+++ src/slam_toolbox_multirobot.cpp
@@ -111,7 +111,7 @@ void MultiRobotSlamToolbox::laserCallback(
     Matrix3 covariance;
     covariance.SetToIdentity();
     publishLocalizedScan(scan, laser->GetOffsetPose(), 
-        range_scan->GetOdometricPose(), covariance, scan->header.stamp);
+        range_scan->GetCorrectedPose(), covariance, scan->header.stamp);
   }
 }
 
